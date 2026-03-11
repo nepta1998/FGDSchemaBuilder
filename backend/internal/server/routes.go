772ca823dd4372ef8c4d40f06da5ba3390c214d8
neptali <njpm28@gmail.com>
2026-03-10 21:58:48 -0400

    adding routes and tests

--- backend/internal/server/routes.go
+++ backend/internal/server/routes.go
@@ -14,10 +14,14 @@ const (
 func RegisterRoutes(mux *http.ServeMux) {
 	// 1. Inicializar los servicios (las herramientas)
 	parserSvc := service.NewParserService()
+	generateSvc := service.NewGenerateService()
 
 	// 2. Inicializar los manejadores e inyectar sus servicios
 	parserHdl := handler.NewParserHandler(parserSvc)
+	generateHdl := handler.NewGenerateHandler(generateSvc)
+
 
 	// 3. Registrar las rutas
 	mux.HandleFunc("POST "+PathParse, parserHdl.Parse)
+	mux.HandleFunc("POST "+PathGenerate, generateHdl.Generate)
 }
